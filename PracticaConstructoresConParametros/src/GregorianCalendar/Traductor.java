dad97a4124f14d920334753b6335042cc9ee8e44
Axel Berlot <berlot83@yahoo.com.ar>
2017-06-05 17:24:20 -0300

    Traductor.java actualizado probá ahora
    Probá ahora, en este branch te dejo el código y voy a hacer un pull request. Funciona muy bien, sin embargo te digo que la clase Date y SimpleDateFormat son más eficientes. Acá tenés solucionado el problema igualmente, no es habitual hacer tantos llamados a distintas clases, pasa que el de P.Inf lo hace a modo de educativo y hace bien, así se aprende pero en tus proyectos tenés que hacer todo más simple porque el día que te olvidás de como lo hiciste, cagáste fuego.

--- PracticaConstructoresConParametros/src/GregorianCalendar/Traductor.java
+++ PracticaConstructoresConParametros/src/GregorianCalendar/Traductor.java
@@ -2,63 +2,103 @@ package GregorianCalendar;
 
 public class Traductor{
 	
-	protected String diaE;
+protected String diaE;
 	protected String mesE;
-	
-
 	
 	public void ResiDiaI(String diaI)   //Setter
 	{
-	if (diaI == "Mon")
-	{this.diaE = "Lun";}
-	else if (diaI == "Tue")
-	{this.diaE = "Mar";}
-	else if (diaI == "Wed")
-	{this.diaE = "Mie";}	
-	else if (diaI == "Thu")
-	{this.diaE = "Jue";}
-	else if (diaI == "Fri")
-	{this.diaE = "Vie";}
-	else if (diaI == "Sat")
-	{this.diaE = "Sab";}
-	else if (diaI == "Sun")
-	{this.diaE = "Dom";}
+	if (diaI.equals("Mon"))
+			{
+				this.diaE = "Lun";
+			}
+	else if (diaI.equals("Tue"))
+			{
+				this.diaE = "Mar";
+			}
+	else if (diaI.equals("Wed"))
+			{
+				this.diaE = "Mie";
+			}	
+	else if (diaI.equals("Thu"))
+			{
+				this.diaE = "Jue";
+			}
+	else if (diaI.equals("Fri"))
+			{
+				this.diaE = "Vie";
+			}
+	else if (diaI.equals("Sat"))
+			{
+				this.diaE = "Sab";
+			}
+	else if (diaI.equals("Sun"))
+			{
+				this.diaE = "Dom";
+			}
 	}
 	
 	public String TraDiaE() //getter
-	{return diaE;}
+		{
+			return diaE;
+		}
 	
 	
 	public void ResiMesE(String mesI)   //setter
 	{	
-		if (mesI == "Jan")
-		{this.mesE = "Ene";}
-		else if (mesI == "Feb")
-		{this.mesE = mesI;}
-		else if(mesI == "Mar")
-		{this.mesE = mesI;}
-		else if (mesI == "Apr")
-		{this.mesE = "Abr";}
-		else if (mesI == "May")
-		{this.mesE = mesI;}
-		else if (mesI == "Jun")
-		{this.mesE = mesI;}
-		else if (mesI == "Jul")
-		{this.mesE = mesI;}
-		else if (mesI == "Agu")
-		{this.mesE = "Ago";}
-		else if (mesI == "Sep")
-		{this.mesE = mesI;}
-		else if (mesI == "Oct")
-		{this.mesE = mesI;}
-		else if (mesI == "Nov")
-		{this.mesE = mesI;}
-		else if (mesI == "Dec")
-		{this.mesE = "Dic";}
+		if (mesI.equals("Jan"))
+			{
+				this.mesE = "Ene";
+			}
+		else if (mesI.equals("Feb"))
+			{
+				this.mesE = mesI;
+			}
+		else if(mesI.equals("Mar"))
+			{
+				this.mesE = mesI;
+			}
+		else if (mesI.equals("Apr"))
+			{
+				this.mesE = "Abr";
+			}
+		else if (mesI.equals("May"))
+			{
+				this.mesE = mesI;
+			}
+		else if (mesI.equals("Jun"))
+			{
+				this.mesE = mesI;
+			}
+		else if (mesI.equals("Jul"))
+			{
+				this.mesE = mesI;
+			}
+		else if (mesI.equals("Agu"))
+			{
+				this.mesE = "Ago";
+			}
+		else if (mesI.equals("Sep"))
+			{
+				this.mesE = mesI;
+			}
+		else if (mesI.equals("Oct"))
+			{
+				this.mesE = mesI;
+			}
+		else if (mesI.equals("Nov"))
+			{
+				this.mesE = mesI;
+			}
+		else if (mesI.equals("Dec"))
+			{
+				this.mesE = "Dic";
+			}
 	}
 
 	
 	public String TraMesE()
-	{return mesE;}
+		{
+			return mesE;
+		}
 	
 }
